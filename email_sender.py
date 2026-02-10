6550c9bf22dceef3aec746182aeb2fe6ae33673e
Shreya <shreyavnadiger@gmail.com>
2026-02-10 10:35:51 +0530

    chat

---
+++ email_sender.py
@@ -0,0 +1,24 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from secrets import sender_email, receiver_email, password
+
+def send_email(receiver_email, content):
+    msg = MIMEMultipart()
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    msg["Subject"] = "hello vaishnavi from shrey"
+    body = content
+    msg.attach(MIMEText(body, "plain"))
+
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(sender_email, password)
+        server.send_message(msg)
+        server.quit()
+        print("Email sent successfully!")
+    except Exception as e:
+        print("Error:", e)
+
+send_email(receiver_email, "hello vaish have a good day.")
---
+++ secrets.py
@@ -0,0 +1,3 @@
+sender_email = "shreyavnadiger@gmail.com"
+receiver_email = "vaishnavilalan@gmail.com"
+password = "befh vtya lipr qtyj"  # use app password if required
